dd9a2ae2586b66833786be0959e32e367f0145be
maty <mfedak@gmail.com>
2015-08-08 09:37:03 +0200

    presentation updated

--- presentation/03_react.js
+++ presentation/03_react.js
@@ -1,26 +1,67 @@
-// Declarative rendering
+// DECLARATIVE PART OF REACT
+
+
+
+
+// Declarative programing
 
 // In computer science, declarative programming is a programming paradigm,
 // a style of building the structure and elements of computer programs,
 // that expresses the logic of a computation
 // without describing its control flow.
 
-// $("#modal").open();
-// $("#warning").show();
 
-// show error
-// $("#warning").hide();
-// $("#error").show();
 
+
+// Imperative UI
+
+// Descibe how ui should be changed
+
+// open modal with warning
+`
+ $("#modal").open();
+ $("#warning").show();
+`;
+// show error that occured
+`
+ $("#warning").hide();
+ $("#error").show();
+`;
 // close modal and error
-// $("#modal").close();
-// $("#warning").hide();
-
-// other aproach just describe what you want
-// and leave calculation to how to do it to comuter
-
+`
+ $("#modal").close();
+ $("#warning").hide();
+`;
 
 
+
+
+
+// Notification example
+
+
+
+
+
+
+
+// Declarative UI
+
+// Describe what you want to render
+// and leave calculation to how to do it to computer
+
+
+
+
+
+
+// Declarative rendering on server
+
+
+
+
+
+// Example of declarative rendering in react
 
 import React from 'react';
 
@@ -29,10 +70,11 @@ const targetElement = document.querySelector('#app');
 
 //React.render(template, targetElement);
 
-const template2 = <div>Other text</div>;
-window.swichT2 = () => React.render(template2, targetElement);
+const template2 = <h1>Other text</h1>;
+//setTimeout(() => React.render(template2, targetElement), 2000);
 
-// let do something with state
+
+// let do something with statefull
 const input1 = (
   <div>
     <input />
@@ -45,5 +87,148 @@ const input2 = (
     second
   </div>
 );
-window.swichI1 = () => React.render(input1, targetElement);
-window.swichI2 = () => React.render(input2, targetElement);
+// React.render(input1, targetElement);
+// setTimeout(() => React.render(input2, targetElement), 2000);
+
+
+// REUSABLE COMPONENTS
+
+// video tag - is in browser
+// modals
+// dropdowns
+// autocomplete
+
+// would be nice if we can define our components
+`
+<Modal open=false dismisable=true>
+  //content
+</Modal>
+`;
+// Api for comunication with custom component:
+// 1. we can specify properties
+// 2. we can specify what is inside components
+
+// Simplest custom component in react just render something
+class SimpleComponent extends React.Component {
+  render() {
+    return <div>Some text inside component</div>;
+  }
+}
+
+
+
+class SimpleComponentWithProps extends React.Component {
+  render() {
+    let text = this.props.text;
+    return <div>{text}</div>;
+  }
+}
+// React.render(
+<SimpleComponentWithProps text="anything">
+  <div>What will happend with child?</div>
+</SimpleComponentWithProps>
+// , targetElement);
+
+
+class SimpleComponentWithChildren extends React.Component {
+  render() {
+    let text = this.props.text;
+    return (
+      <div>
+        <h1>{text}</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+// React.render(
+<SimpleComponentWithChildren text="nejaky text">
+  <p> This text will be inside </p>
+</SimpleComponentWithChildren>
+// , targetElement);
+
+
+
+class SimpleComponentWithState extends React.Component {
+
+  state = {
+    counter: 0
+  }
+
+  increaseCounter = () => this.setState({counter: this.state.counter + 1});
+
+  render() {
+    return (
+      <div>
+        {this.state.counter}
+        <button onClick={this.increaseCounter}>increase</button>
+      </div>
+    );
+  }
+}
+// React.render(
+<SimpleComponentWithState />
+// , targetElement);
+
+
+
+class ReadingValuseFromDom extends React.Component {
+  state = {
+    text: "default text"
+  }
+  onTextChange = (e) => {
+    this.setState({text: e.target.value});
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.state.text}
+               onChange={this.onTextChange}>
+        </input>
+        <button onClick={() => alert(this.state.text)}>
+          show
+        </button>
+      </div>
+    );
+  }
+}
+// React.render(
+<ReadingValuseFromDom />
+// , targetElement);
+
+
+
+class LifecycleComponent extends React.Component {
+
+  // First render
+  componentWillMount(){}
+  // render
+  componentDidMount(){}
+
+
+  // On change of props
+  componentWillReceiveProps(nextProps){}
+  shouldComponentUpdate(nextProps, nextState) {}
+  componentWillUpdate(nextProps, nextState) {}
+  //render
+  componentDidUpdate(prevProps, prevState) {}
+
+
+  /* On change of state - almost same as on change
+     of props only componentWillReceiveProps
+     is not called */
+  // shouldComponentUpdate
+  // componentWillUpdate
+  // render
+  // componentDidUpdate
+
+  // Before removing component from tree
+  componentWillUnmount() {}
+
+  render() {
+    return <div> LifecycleComponent </div>;
+  }
+}
+// React.render(
+<LifecycleComponent />
+// , targetElement);
---
+++ presentation/05_advaced.js
@@ -0,0 +1,23 @@
+// Routing in react
+https://github.com/rackt/react-router
+
+// Drag and drop in react
+https://github.com/gaearon/react-dnd
+
+// Animation in react
+https://github.com/chenglou/react-motion
+
+// CSS modules
+https://github.com/css-modules/css-modules
+
+// Bootstrap for react
+http://react-bootstrap.github.io/
+
+// Why REST is not best model
+http://www.infoq.com/presentations/domain-driven-architecture
+
+// More exotic alternative to flux
+http://cycle.js.org/
+
+// More exotic language to build ui
+http://elm-lang.org/
